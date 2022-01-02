08f156a9a728b773ec9a37cbb335550645025528
Kapil Kathuria <kapil.kathuria@gmail.com>
2022-01-02 16:49:40 +0530

    newsletter body WIP. 1 header and single line body looking good

--- lib/src/feature/item_details_view.dart
+++ lib/src/feature/item_details_view.dart
@@ -2,18 +2,57 @@ import 'package:flutter/material.dart';
 
 /// Displays detailed information about a SampleItem.
 class SampleItemDetailsView extends StatelessWidget {
-  const SampleItemDetailsView({Key? key}) : super(key: key);
+  SampleItemDetailsView({Key? key}) : super(key: key);
 
   static const routeName = '/sample_item';
+  final String nlTopic = 'Bachelor in Design (B.Des) (10+2 Any stream)';
+  final nlBody = [
+    'What is B.Des all about ?',
+    '• It is 4 years undergraduate designing degree...'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bachelor in Design (B.Des) (10+2 Any stream)'),
-      ),
-      body: const Center(
-        child: Text('What is B.Des all about ?'),
+        appBar: AppBar(
+          title: Text(nlTopic),
+        ),
+        body: NewsletterTopicHeader(
+          headerText: nlBody[0],
+          bodyText: nlBody[1],
+        ));
+  }
+}
+
+class NewsletterTopicHeader extends StatelessWidget {
+  const NewsletterTopicHeader({
+    Key? key,
+    required this.headerText,
+    required this.bodyText,
+  }) : super(key: key);
+  final String headerText;
+  final String bodyText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            headerText,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            bodyText,
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
       ),
     );
   }
